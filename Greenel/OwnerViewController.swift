ab46be76f0050976c028f0d15b8980c3732f3c59
Andrew Kozlov <a.kozlov@bankex.com>
2018-11-10 15:01:00 +0300

    OwnerViewController list implemented

--- Greenel/OwnerViewController.swift
+++ Greenel/OwnerViewController.swift
@@ -10,4 +10,52 @@ import UIKit
 
 class OwnerViewController: UITableViewController {
     
+    var records: [Record] = []
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        updateRecords()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(updateRecords), name: Service.recordsUpdated, object: nil)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc private func updateRecords() {
+        records = Service.shared.records
+        
+        updateView()
+    }
+    
+    private func updateView() {
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return records.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let record = records[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RecordCell", for: indexPath)
+        cell.textLabel?.text = "\(record.certificateNumber) (\(record.type))"
+        cell.detailTextLabel?.text = record.ownerName
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ShowRecord", sender: nil)
+    }
+    
 }

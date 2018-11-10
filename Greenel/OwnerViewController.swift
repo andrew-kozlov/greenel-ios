b4235ba7dee9dda7f9e5d73eff53f72f022567af
Andrew Kozlov <a.kozlov@bankex.com>
2018-11-10 13:23:54 +0300

    OwnerViewController added

---
+++ Greenel/OwnerViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OwnerViewController.swift
+//  Greenel
+//
+//  Created by Andrew Kozlov on 10/11/2018.
+//  Copyright © 2018 Andrew Kozlov. All rights reserved.
+//
+
+import UIKit
+
+class OwnerViewController: UITableViewController {
+    
+}

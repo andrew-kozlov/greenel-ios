a3cfe5de6056a1f90e1006bde98f02a9369e66bc
Andrew Kozlov <a.kozlov@bankex.com>
2018-11-10 21:21:03 +0300

    Appearance updated

--- Greenel/AppDelegate.swift
+++ Greenel/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         Service.shared.fetch()
         
+        window?.tintColor = UIColor(red: CGFloat(17.0 / 255.0), green: CGFloat(147.0 / 255.0), blue: CGFloat(0.0 / 255.0), alpha: 1.0)
+        
         return true
     }
     

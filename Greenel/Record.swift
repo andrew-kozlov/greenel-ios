d61f519dd8d2b7591164461f9c73263a0c937592
Andrew Kozlov <a.kozlov@bankex.com>
2018-11-10 14:06:29 +0300

    Record added

---
+++ Greenel/Record.swift
@@ -0,0 +1,26 @@
+//
+//  Record.swift
+//  Greenel
+//
+//  Created by Andrew Kozlov on 10/11/2018.
+//  Copyright © 2018 Andrew Kozlov. All rights reserved.
+//
+
+import Foundation
+
+struct Record {
+    
+    let identifier: String
+    let creationDate: Date
+    let ownerName: String
+    let ownerOGRN: String
+    let ownerAddress: String
+    let volume: Float
+    let period: String
+    let emissionVolume: Float
+    let certificateNumber: String
+    let lifeTime: Int
+    let type: String
+    let wallet: String
+    
+}
